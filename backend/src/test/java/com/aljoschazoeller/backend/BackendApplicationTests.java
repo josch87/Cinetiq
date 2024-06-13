08f6a3fa6230efc9c0edad7b9dd405bbd9d663b1
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-13 11:08:05 +0200

    Initial commit

---
+++ backend/src/test/java/com/aljoschazoeller/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
