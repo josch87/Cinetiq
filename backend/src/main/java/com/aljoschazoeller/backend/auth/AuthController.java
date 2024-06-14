9b1d6c05796681b788b00f0e44e47c95f58360d0
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-14 09:20:03 +0200

    Update AuthController to return GithubUser object
    The `/me` endpoint of `AuthController` has been updated to return a `GithubUser` object rather than a string with the username. A `GithubUser` class was added to hold user's GitHub profile information. Additional tests for the updated endpoint were also written. Changes were also made to `pom.xml` to accommodate this adjustment.

--- backend/src/main/java/com/aljoschazoeller/backend/auth/AuthController.java
+++ backend/src/main/java/com/aljoschazoeller/backend/auth/AuthController.java
@@ -1,17 +1,30 @@
 package com.aljoschazoeller.backend.auth;
 
+import com.aljoschazoeller.backend.auth.domain.GithubUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @GetMapping("/me")
-    public String getMe(@AuthenticationPrincipal OAuth2User user) {
-        return user.getAttributes().get("login").toString();
+    public GithubUser getMe(@AuthenticationPrincipal OAuth2User user) {
+        Map<String, Object> attributes = user.getAttributes();
+
+        return new GithubUser(
+                attributes.get("id").toString(),
+                attributes.get("login") != null ? attributes.get("login").toString() : null,
+                attributes.get("avatar_url") != null ? attributes.get("avatar_url").toString() : null,
+                attributes.get("name") != null ? attributes.get("name").toString() : null,
+                attributes.get("company") != null ? attributes.get("company").toString() : null,
+                attributes.get("location") != null ? attributes.get("location").toString() : null,
+                attributes.get("bio") != null ? attributes.get("bio").toString() : null
+        );
     }
 }
