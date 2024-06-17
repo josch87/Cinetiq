cdd497aea45667eca8811d98d4a9f14374ad9098
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-17 16:41:26 +0200

    Add DashboardWidget to DashboardPage
    A new component, DashboardWidget, has been created and added to the DashboardPage. The layout of the DashboardPage has been modified to accommodate this new component using a CSS Grid structure.

---
+++ frontend/src/components/DashboardWidget/DashboardWidget.tsx
@@ -0,0 +1,3 @@
+export default function DashboardWidget() {
+  return <>Widget</>;
+}
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,7 @@
 import DefaultPageTemplate from "./templates/DefaultPageTemplate.tsx";
 import { githubUserType } from "../model/userModel.ts";
+import { Grid, GridItem } from "@chakra-ui/react";
+import DashboardWidget from "../components/DashboardWidget/DashboardWidget.tsx";
 
 type DashboardPageProps = {
   user: githubUserType | null | undefined;
@@ -12,7 +14,21 @@ export default function DashboardPage({ user }: Readonly<DashboardPageProps>) {
       pageSubtitle="Quick access and overview of all festival data"
       user={user}
     >
-      Dashboard Content
+      <Grid
+        templateAreas={`"main side"
+                        "main side"
+                        "main side"`}
+        gridTemplateRows={"50px 50px 50px"}
+        gridTemplateColumns={"2fr 1fr"}
+        gap={4}
+      >
+        <GridItem w="100%" h="10">
+          <DashboardWidget />
+        </GridItem>
+        <GridItem w="100%" h="10">
+          test4
+        </GridItem>
+      </Grid>
     </DefaultPageTemplate>
   );
 }
