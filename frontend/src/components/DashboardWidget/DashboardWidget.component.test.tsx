d1ffc5c7403c21e0390fad53935e1aa743f53652
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-18 17:51:48 +0200

    Add tests for DashboardWidget component
    The tests have been implemented for the DashboardWidget component. The tests ensure that the DashboardWidget is rendering the correct heading and its children. These unit tests bring us closer to maintaining a stable and bug-free codebase.

---
+++ frontend/src/components/DashboardWidget/DashboardWidget.component.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import DashboardWidget from "./DashboardWidget.tsx";
+
+test("DashboardWidget is rendering a heading", () => {
+  render(
+    <DashboardWidget heading="How to rule the world">
+      <p>Ruling the world is not easy.</p>
+    </DashboardWidget>
+  );
+  const heading = screen.getByRole("heading", {
+    level: 2,
+    name: /how to rule the world/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
+
+test("DashboardWidget is rendering it's children", () => {
+  render(
+    <DashboardWidget heading="How to rule the world">
+      <p>Ruling the world is not easy.</p>
+    </DashboardWidget>
+  );
+  const children = screen.getByText(/ruling the world is not easy/i);
+  expect(children).toBeInTheDocument();
+});
