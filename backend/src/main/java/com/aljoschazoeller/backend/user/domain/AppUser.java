0c7f27339a164a392a88e361f87d2b997595eff1
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-19 12:03:21 +0200

    Add UserService tests and update AppUser model
    Implemented unit tests for the UserService class and updated the AppUser model by changing githubUserProfile from an Object type to a Map. These tests will ensure the correct functioning of UserService methods. The adjustment in AppUser will allow a more flexible representation of the user's Github profile.

--- backend/src/main/java/com/aljoschazoeller/backend/user/domain/AppUser.java
+++ backend/src/main/java/com/aljoschazoeller/backend/user/domain/AppUser.java
@@ -4,13 +4,14 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.Map;
 
 @Document("appUsers")
 public record AppUser(
         @Id
         String id,
         String githubId,
-        Object githubUserProfile, // Profile during first signup
+        Map<String, Object> githubUserProfile, // Profile during first signup
         Instant createdAt
 ) {
 }
