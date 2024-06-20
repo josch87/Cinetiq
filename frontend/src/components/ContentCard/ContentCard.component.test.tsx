427d2884d326c3e60bca2f630709d095f56e4018
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-20 23:16:59 +0200

    Add series-related tests in ContentCard component
    Additional tests have been included for the 'series' content type within the ContentCard component. This includes tests for rendering of the original title, content type icon, and tooltip. The existing tests have also been updated for clarity and consistency.

--- frontend/src/components/ContentCard/ContentCard.component.test.tsx
+++ frontend/src/components/ContentCard/ContentCard.component.test.tsx
@@ -32,6 +32,20 @@ const contentExhibition: contentType = {
   },
 };
 
+const contentSeries: contentType = {
+  id: "1",
+  contentType: "SERIES",
+  originalTitle: "Original title",
+  englishTitle: "",
+  germanTitle: "",
+  createdAt: new Date("2024-06-20T18:20:05.208Z"),
+  createdBy: {
+    id: "appUser-id-1",
+    githubId: "github-id-1",
+    createdAt: new Date("2024-06-10T15:10:05.217Z"),
+  },
+};
+
 test("ContentCard renders the english title of the content", () => {
   render(<ContentCard content={contentMovie} />);
   const title = screen.getByText(/english title/i);
@@ -44,9 +58,21 @@ test("ContentCard renders the german title of the content", () => {
   expect(title).toBeInTheDocument();
 });
 
+test("ContentCard renders the original title of the content", () => {
+  render(<ContentCard content={contentSeries} />);
+  const title = screen.getByText(/original title/i);
+  expect(title).toBeInTheDocument();
+});
+
 test("ContentCard renders a svg icon for the content type 'movie'", () => {
   render(<ContentCard content={contentMovie} />);
   const svgIcon = screen.getByTestId("svg-icon-contenttype-movie");
+  expect(svgIcon).toBeInTheDocument();
+});
+
+test("ContentCard renders a svg icon for the content type 'series'", () => {
+  render(<ContentCard content={contentSeries} />);
+  const svgIcon = screen.getByTestId("svg-icon-contenttype-series");
   expect(svgIcon).toBeInTheDocument();
 });
 
@@ -56,10 +82,26 @@ test("ContentCard renders a svg icon for the content type 'exhibition'", () => {
   expect(svgIcon).toBeInTheDocument();
 });
 
-test("ContentCard renders a tooltip for the content type on icon hover", async () => {
+test("ContentCard renders a tooltip for the content type 'movie' on icon hover", async () => {
   render(<ContentCard content={contentMovie} />);
   const svgIcon = screen.getByTestId("svg-icon-contenttype-movie");
   await userEvent.hover(svgIcon);
   const tooltipElement = screen.getByText(/movie/i);
+  expect(tooltipElement).toBeInTheDocument();
+});
+
+test("ContentCard renders a tooltip for the content type 'Series' on icon hover", async () => {
+  render(<ContentCard content={contentSeries} />);
+  const svgIcon = screen.getByTestId("svg-icon-contenttype-series");
+  await userEvent.hover(svgIcon);
+  const tooltipElement = screen.getByText(/series/i);
+  expect(tooltipElement).toBeInTheDocument();
+});
+
+test("ContentCard renders a tooltip for the content type 'Exhibition' on icon hover", async () => {
+  render(<ContentCard content={contentExhibition} />);
+  const svgIcon = screen.getByTestId("svg-icon-contenttype-exhibition");
+  await userEvent.hover(svgIcon);
+  const tooltipElement = screen.getByText(/exhibition/i);
   expect(tooltipElement).toBeInTheDocument();
 });
