f115ba150438732a428759feb491fded458853e2
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-20 20:03:21 +0200

    Switch no-data-dogs with svg

--- frontend/src/components/NoData/NoData.tsx
+++ frontend/src/components/NoData/NoData.tsx
@@ -2,9 +2,9 @@ import { Flex, Image, Text } from "@chakra-ui/react";
 
 function getRandomDog() {
   const dogs: string[] = [
-    "no-data-dog-001.jpg",
-    "no-data-dog-002.jpg",
-    "no-data-dog-003.jpg",
+    "no-data-dog-001.svg",
+    "no-data-dog-002.svg",
+    "no-data-dog-003.svg",
   ];
 
   const randomDog = dogs[Math.floor(Math.random() * dogs.length)];
