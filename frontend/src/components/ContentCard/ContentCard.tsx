f201e9125bbbe5ef4750d67fef75e38620889f1b
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-21 12:42:03 +0200

    Add navigation to ContentCard component
    Implemented a click event on the ContentCard component in the frontend. This now navigates to the specific content page when the card is clicked, enhancing user interaction. The "useNavigate" hook from react-router-dom was utilized to achieve this.

--- frontend/src/components/ContentCard/ContentCard.tsx
+++ frontend/src/components/ContentCard/ContentCard.tsx
@@ -2,14 +2,17 @@ import { Card, CardHeader, Heading } from "@chakra-ui/react";
 import { contentType } from "../../model/contentModel.ts";
 import ContentCardTitle from "./ContentCardTitle.tsx";
 import ContentCardTypeIcon from "./ContentCardTypeIcon.tsx";
+import { useNavigate } from "react-router-dom";
 
 type ContentCardProps = {
   content: contentType;
 };
 
 export default function ContentCard({ content }: Readonly<ContentCardProps>) {
+  const navigate = useNavigate();
+
   return (
-    <Card>
+    <Card onClick={() => navigate(`/content/${content.id}`)} cursor="pointer">
       <CardHeader
         display="flex"
         flexDirection="row"
