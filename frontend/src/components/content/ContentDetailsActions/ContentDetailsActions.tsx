1df464dddb6ca2ed3dc0235930bcd6dd47ae8b52
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-22 10:24:56 +0200

    Rename anonymous function to ContentDetailsActions
    The anonymous function in ContentDetailsActions.tsx has been given a specific name, ContentDetailsActions, to make the code more readable and easier to debug.

--- frontend/src/components/content/ContentDetailsActions/ContentDetailsActions.tsx
+++ frontend/src/components/content/ContentDetailsActions/ContentDetailsActions.tsx
@@ -2,7 +2,7 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { FaFileExport, FaShareNodes, FaTrash } from "react-icons/fa6";
 import { FiChevronDown } from "react-icons/fi";
 
-export default function () {
+export default function ContentDetailsActions() {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<FiChevronDown />}>
