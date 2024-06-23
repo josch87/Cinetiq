c0ae45f5e89241034b4d27e21903eb933efd8df9
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-23 19:02:36 +0200

    Add testing for ContentDetailsHeader component
    A test has been added to the ContentDetailsHeader component. This test checks that the component renders the English title of the content correctly. The test uses the Jest and Testing Library utilities for effective testing.

---
+++ frontend/src/components/content/ContentDetailsHeader/ContentDetailsHeader.component.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ContentDetailsHeader from "./ContentDetailsHeader.tsx";
+import { contentType } from "../../../model/contentModel.ts";
+
+const content: contentType = {
+  id: "1",
+  contentType: "MOVIE",
+  originalTitle: "Original title of 1",
+  englishTitle: "English title of 1",
+  germanTitle: "German title of 1",
+  createdAt: new Date("2024-06-20T18:20:05.208Z"),
+  createdBy: {
+    id: "appUser-id-1",
+    githubId: "github-id-1",
+    createdAt: new Date("2024-06-10T15:10:05.217Z"),
+  },
+};
+
+test("ContentDetailsHeader renders the english title", () => {
+  render(<ContentDetailsHeader content={content} />);
+  const title = screen.getByRole("heading", {
+    level: 2,
+    name: /english title of 1/i,
+  });
+  expect(title).toBeInTheDocument();
+});
