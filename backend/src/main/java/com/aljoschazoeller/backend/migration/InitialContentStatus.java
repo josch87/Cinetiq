91c55392e58ae4d5251daa0b7be4e73f4d2c6abb
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-24 11:33:50 +0200

    Add InitialContentStatus migration class
    This newly added InitialContentStatus class is responsible for a database migration task. It sets the status of all content that presently don't have a status to 'ACTIVE'. It also defines the corresponding rollback operation to nullify the status if necessary.

---
+++ backend/src/main/java/com/aljoschazoeller/backend/migration/InitialContentStatus.java
@@ -0,0 +1,36 @@
+package com.aljoschazoeller.backend.migration;
+
+import com.aljoschazoeller.backend.content.domain.Content;
+import com.aljoschazoeller.backend.content.domain.ContentStatus;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@ChangeUnit(id="initial-contentstatus", order="1", author="Aljoscha Zöller" )
+public class InitialContentStatus {
+
+    private final MongoTemplate mongoTemplate;
+
+    public InitialContentStatus(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Execution
+    public void execution() {
+        mongoTemplate.findAll(Content.class)
+                .stream()
+                .filter((content -> content.status() == null))
+                .map(content -> content.withStatus(ContentStatus.ACTIVE))
+                .forEach(mongoTemplate::save);
+    }
+
+    @RollbackExecution
+    public void rollbackExecution() {
+        mongoTemplate.findAll(Content.class)
+                .stream()
+                .filter((content -> content.status() == ContentStatus.ACTIVE))
+                .map(content -> content.withStatus(null))
+                .forEach(mongoTemplate::save);
+    }
+}
