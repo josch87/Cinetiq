a1082688ce5bd36291b6fc56fb45c32d0b972904
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-24 10:59:26 +0200

    Enable Mongock for MongoDB migrations
    Mongock dependencies have been added to the pom.xml and it has been enabled in the BackendApplication.java class for managing MongoDB migrations. The migration path is specified in the application.properties file.

--- backend/src/main/java/com/aljoschazoeller/backend/BackendApplication.java
+++ backend/src/main/java/com/aljoschazoeller/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.aljoschazoeller.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongock
 @SpringBootApplication
 public class BackendApplication {
 
