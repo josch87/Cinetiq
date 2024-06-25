6bdd90226dd66d8a3d1c68e4cea2c5574ab0a0d0
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-25 17:15:08 +0200

    Add form fields to ContentCreationDrawer
    The ContentCreationDrawer component has been updated with form fields to allow input for the content's type, English title, German title, and original title. The form is also enhanced with error messages to ensure each field is filled before submission.

--- frontend/src/components/content/ContentCreationDrawer/ContentCreationDrawer.tsx
+++ frontend/src/components/content/ContentCreationDrawer/ContentCreationDrawer.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -7,7 +8,12 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
   Input,
+  Select,
+  Stack,
 } from "@chakra-ui/react";
 import { useContentCreationDrawerStore } from "../../../store/store.ts";
 
@@ -26,7 +32,47 @@ export default function ContentCreationDrawer() {
         <DrawerHeader>Create new content</DrawerHeader>
 
         <DrawerBody>
-          <Input placeholder="Type here..." />
+          <Box as="form">
+            <Stack spacing={5}>
+              <FormControl isRequired>
+                <FormLabel color="teal.500">Content Type</FormLabel>
+                <Select placeholder="Select a type">
+                  <option value="MOVIE">Movie</option>
+                  <option value="SERIES">Series</option>
+                  <option value="EXHIBITION">Exhibition</option>
+                </Select>
+              </FormControl>
+              <Stack
+                as="fieldset"
+                px={4}
+                pb={4}
+                borderWidth={2}
+                borderRadius="md"
+                spacing={4}
+              >
+                <FormLabel as="legend" fontSize="lg" color="teal.500">
+                  Titles
+                </FormLabel>
+                <FormControl id="englishTitle" isRequired>
+                  <FormLabel>English Title</FormLabel>
+                  <Input type="text" />
+                  <FormErrorMessage>English title is required</FormErrorMessage>
+                </FormControl>{" "}
+                <FormControl id="germanTitle" isRequired>
+                  <FormLabel>German Title</FormLabel>
+                  <Input type="text" />
+                  <FormErrorMessage>German title is required</FormErrorMessage>
+                </FormControl>{" "}
+                <FormControl id="originalTitle" isRequired>
+                  <FormLabel>Original Title</FormLabel>
+                  <Input type="text" />
+                  <FormErrorMessage>
+                    Original title is required
+                  </FormErrorMessage>
+                </FormControl>
+              </Stack>
+            </Stack>
+          </Box>
         </DrawerBody>
 
         <DrawerFooter>
