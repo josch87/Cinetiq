632d17df8e1484ba2189c07587fb2612a1ffec73
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-26 17:24:49 +0200

    Add validation for NewContentDTO fields
    A validation constraint @NotBlank has been added to the 'originalTitle' field in NewContentDTO to ensure it contains at least one non-whitespace character. Additionally, the @Valid annotation has been included in the 'createContent' method of 'ContentController' to apply these validations. A dependency for 'spring-boot-starter-validation' has also been added to pom.xml.

--- backend/src/main/java/com/aljoschazoeller/backend/content/domain/NewContentDTO.java
+++ backend/src/main/java/com/aljoschazoeller/backend/content/domain/NewContentDTO.java
@@ -1,7 +1,10 @@
 package com.aljoschazoeller.backend.content.domain;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record NewContentDTO(
         ContentType contentType,
+        @NotBlank(message = "The Original Title must have at least one non-whitespace character")
         String originalTitle,
         String englishTitle,
         String germanTitle
