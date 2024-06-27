03bd708f4db97c82b6f019bf18a5568113240dc4
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-27 13:21:56 +0200

    Remove ContentTypeDeserializer and InvalidContentTypeException
    The unused classes ContentTypeDeserializer and InvalidContentTypeException have been deleted. These classes were not necessary for the current operations of the backend and hence have been removed from the codebase to clean it up.

--- backend/src/main/java/com/aljoschazoeller/backend/exceptions/InvalidContentTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.aljoschazoeller.backend.exceptions;
-
-public class InvalidContentTypeException extends RuntimeException {
-    public InvalidContentTypeException(String message) {
-        super(message);
-    }
-}
