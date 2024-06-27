7f4e6b9f47cef276824d5059c58c050d43ecb1b0
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-06-27 15:47:09 +0200

    Move CancelContentCreationAlertDialog component to AlertDialogs folder
    The CancelContentCreationAlertDialog component and its associated tests have been moved to the AlertDialogs folder for better organization. This change supports the project convention of grouping similar components together.

--- frontend/src/components/content/ContentCreationDrawer/ContentCreationDrawer.tsx
+++ frontend/src/components/content/ContentCreationDrawer/ContentCreationDrawer.tsx
@@ -27,7 +27,7 @@ import { ContentType, NewContentType } from "../../../model/contentModel.ts";
 import { useRef } from "react";
 import CancelContentCreationAlertDialog, {
   CancelAlertDialogDisclosureType,
-} from "../CancelContentCreationAlertDialog/CancelContentCreationAlertDialog.tsx";
+} from "../../AlertDialogs/CancelContentCreationAlertDialog/CancelContentCreationAlertDialog.tsx";
 
 export default function ContentCreationDrawer() {
   const toast = useToast();
