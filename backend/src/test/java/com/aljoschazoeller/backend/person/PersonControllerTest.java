5bad717b9529f20c622ee31e60f42e394d54e87d
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-07-03 21:16:46 +0200

    Add test cases for PersonController
    Added test cases in the PersonControllerTest class to verify the behaviors of different scenarios including when a user is not authenticated, when the person exists in the database, and when the person does not exist in the database. This will help ensure that the application behaves as expected under these conditions.

--- backend/src/test/java/com/aljoschazoeller/backend/person/PersonControllerTest.java
+++ backend/src/test/java/com/aljoschazoeller/backend/person/PersonControllerTest.java
@@ -33,18 +33,105 @@ class PersonControllerTest {
     private UserRepository userRepository;
 
     @Test
-    void createPersonTest_whenNotAuthenticated_thenReturnUnauthorized() throws Exception{
-        mockMvc.perform(post("/api/people")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                        {
-                            "firstName": "Chuck",
-                            "lastName": "Norris"
-                        }
-                        """))
+    void getPersonByIdTest_whenNotAuthenticated_thenReturnUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/people/1"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(content().string(""));
     }
+
+    @Test
+    @WithMockUser
+    @DirtiesContext
+    void getPersonByIdTest_whenPersonInDatabase_thenReturnPerson() throws Exception {
+        AppUser user = AppUser.builder()
+                .id("appUser-id-1")
+                .githubId("user")
+                .createdAt(Instant.parse("2024-06-20T15:10:05.022Z"))
+                .build();
+
+        userRepository.save(user);
+
+        MvcResult result = mockMvc.perform(post("/api/people")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "firstName": "Chuck",
+                                    "lastName": "Norris"
+                                }
+                                """))
+                .andExpect(status().isCreated())
+                .andExpect(content().json("""
+                        {
+                            "info": {
+                                "count": null
+                            },
+                            "data": {
+                                "status": "ACTIVE",
+                                "statusUpdatedAt": null,
+                                "statusUpdatedBy": null,
+                                "firstName": "Chuck",
+                                "lastName": "Norris",
+                                "createdBy": {
+                                    "id": "appUser-id-1",
+                                    "githubId": "user",
+                                    "createdAt": "2024-06-20T15:10:05.022Z"
+                                }
+                            }
+                        }
+                        """))
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        ApiResponse<Person> apiResponse = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+        Person returnedPerson = apiResponse.getData();
+
+        mockMvc.perform(get("/api/people/" + returnedPerson.id()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "info": {
+                                "count": null
+                            },
+                            "data": {
+                                "status": "ACTIVE",
+                                "statusUpdatedAt": null,
+                                "statusUpdatedBy": null,
+                                "firstName": "Chuck",
+                                "lastName": "Norris",
+                                "createdBy": {
+                                    "id": "appUser-id-1",
+                                    "githubId": "user",
+                                    "createdAt": "2024-06-20T15:10:05.022Z"
+                                }
+                            }
+                        }
+                        """));
+    }
+
+    @Test
+    @WithMockUser
+    void getPersonById_whenPersonNotInDatabase_thenReturnNotFound() throws Exception {
+        mockMvc.perform(get("/api/people/non-existing-id"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("No person found with ID 'non-existing-id'"));
+    }
+
+    @Test
+    void createPersonTest_whenNotAuthenticated_thenReturnUnauthorized() throws Exception {
+        mockMvc.perform(post("/api/people")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "firstName": "Chuck",
+                                    "lastName": "Norris"
+                                }
+                                """))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string(""));
+    }
+
     @Test
     @WithMockUser
     @DirtiesContext
@@ -58,13 +145,13 @@ class PersonControllerTest {
         userRepository.save(user);
 
         MvcResult result = mockMvc.perform(post("/api/people")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                        {
-                            "firstName": "Chuck",
-                            "lastName": "Norris"
-                        }
-                        """))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "firstName": "Chuck",
+                                    "lastName": "Norris"
+                                }
+                                """))
                 .andExpect(status().isCreated())
                 .andExpect(content().json("""
                         {
