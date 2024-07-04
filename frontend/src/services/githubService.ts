ea4d110a46641b588b26081120845bd0f338c991
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-07-04 22:00:58 +0200

    Remove SWR package and related functionalities
    The SWR package and its associated functionalities have been removed from the application. This also involved removing related imports from various components, and modifying the components to not rely on this package. Notably, the `useGithubUserById` function was deleted and no longer used in components.

--- frontend/src/services/githubService.ts
+++ frontend/src/services/githubService.ts
@@ -1,9 +1,4 @@
-import axios from "axios";
-import useSWR from "swr";
-import {
-  GithubUserAuthType,
-  GithubUserSyncedType,
-} from "../model/githubModel.ts";
+import { GithubUserSyncedType } from "../model/githubModel.ts";
 
 export function processSingleGithubUserSynced(
   rawGithubUser: GithubUserSyncedType
@@ -13,20 +8,4 @@ export function processSingleGithubUserSynced(
     created_at: rawGithubUser.created_at,
     updated_at: rawGithubUser.updated_at,
   };
-}
-
-export function useGithubUserById(githubId: string | undefined): {
-  githubUser: GithubUserAuthType | undefined;
-  isLoading: boolean;
-  isError: Error | undefined;
-} {
-  const fetcher = (githubId: string) =>
-    axios
-      .get(`https://api.github.com/user/${githubId}`)
-      .then((response) => response.data)
-      .catch((error) => console.error(error.message));
-
-  const { data, error, isLoading } = useSWR(githubId, fetcher);
-
-  return { githubUser: data, isLoading, isError: error };
 }
