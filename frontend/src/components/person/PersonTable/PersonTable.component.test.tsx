d6982d756046a9d0b87fe97d66aa1c5c53783c5e
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-07-04 16:26:19 +0200

    Wrap PersonTable tests in MemoryRouter
    The person table tests have been wrapped within the MemoryRouter. Since Router context is needed for components that use the "Link" or "Route" components from "react-router-dom", running these tests without the MemoryRouter can cause them to fail.

--- frontend/src/components/person/PersonTable/PersonTable.component.test.tsx
+++ frontend/src/components/person/PersonTable/PersonTable.component.test.tsx
@@ -2,21 +2,34 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import PersonTable from "./PersonTable.tsx";
 import { peopleArray } from "../../../model/personTestData.ts";
+import { MemoryRouter } from "react-router-dom";
 
 test("PersonTable renders the last name of the user", () => {
-  render(<PersonTable people={peopleArray} />);
+  render(
+    <MemoryRouter>
+      <PersonTable people={peopleArray} />
+    </MemoryRouter>
+  );
   const name = screen.getByText(/norris/i);
   expect(name).toBeInTheDocument();
 });
 
 test("PersonTable renders the first name of the user", () => {
-  render(<PersonTable people={peopleArray} />);
+  render(
+    <MemoryRouter>
+      <PersonTable people={peopleArray} />
+    </MemoryRouter>
+  );
   const name = screen.getByText(/chuck/i);
   expect(name).toBeInTheDocument();
 });
 
 test("PersonTable renders the status of the user", () => {
-  render(<PersonTable people={peopleArray} />);
+  render(
+    <MemoryRouter>
+      <PersonTable people={peopleArray} />
+    </MemoryRouter>
+  );
   const name = screen.getAllByText(/active/i);
   expect(name).toHaveLength(2);
 });
