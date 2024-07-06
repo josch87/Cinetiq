a3e9913472ad1a9b03995748c6f25dfabb3c59ca
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-07-07 00:48:46 +0200

    Add logging to service classes
    The service classes (UserService, PersonService, ContentService) were updated to include logging via the slf4j logger. These logs provide warnings when users, persons, or content can't be located, and also log errors and successful operations, improving visibility and debugging capabilities.

--- backend/src/main/java/com/aljoschazoeller/backend/user/UserService.java
+++ backend/src/main/java/com/aljoschazoeller/backend/user/UserService.java
@@ -3,6 +3,8 @@ package com.aljoschazoeller.backend.user;
 import com.aljoschazoeller.backend.auth.GithubMapper;
 import com.aljoschazoeller.backend.exceptions.UserNotFoundException;
 import com.aljoschazoeller.backend.user.domain.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ import java.util.List;
 
 @Service
 public class UserService {
-
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -24,12 +26,18 @@ public class UserService {
 
     public AppUser getAppUserById(String id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("No appUser found with ID '" + id + "'"));
+                .orElseThrow(() -> {
+                    log.warn("Could not find appUser with ID '{}'", id);
+                    return new UserNotFoundException("No appUser found with ID '" + id + "'");
+                });
     }
 
     public AppUser findByGithubId(String githubId) {
         return userRepository.findAppUserByGithubId(githubId)
-                .orElseThrow(() -> new UserNotFoundException("No appUser found with GitHub ID '" + githubId + "'"));
+                .orElseThrow(() -> {
+                    log.warn("Could not find appUser with GitHub ID '{}'", githubId);
+                    return new UserNotFoundException("No appUser found with GitHub ID '" + githubId + "'");
+                });
     }
 
     public boolean checkIfUserExistsById(String id) {
